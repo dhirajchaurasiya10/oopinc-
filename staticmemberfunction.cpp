d4ec4f1ab545123e13a9ddbc99cbac1755ca8ac6
Binod Subedi <bidhaksubedi77@gmail.com>
2024-03-17 18:40:15 +0530

    Update staticmemberfunction.cpp
    There is a minor issue. In the increasecount() function of the book class, you need to return an integer value, since the function has a return type of int.

--- staticmemberfunction.cpp
+++ staticmemberfunction.cpp
@@ -1,23 +1,29 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
-class book{
-	static int count;
-	public:
-		static int getcount(){
-			return count;
-		}
-		static int increasecount() {
-			count++;
-		}
+
+class book {
+    static int count;
+public:
+    static int getcount() {
+        return count;
+    }
+    static int increasecount() {
+        count++;
+        return count; // Add this line to return the updated count
+    }
 };
+
 int book::count;
-int main(){
-	cout<<"initial value"<<book::getcount()<<endl;
-	book b1,b2,b3;
-	book::increasecount();
-	book::increasecount();
-	book::increasecount();
-	book::increasecount();
-	cout<<"final value"<<book::getcount()<<endl;
+
+int main() {
+    cout << "Initial value: " << book::getcount() << endl;
+    book b1, b2, b3;
+    book::increasecount();
+    book::increasecount();
+    book::increasecount();
+    book::increasecount();
+    cout << "Final value: " << book::getcount() << endl;
+    getch(); // Using getch() to hold the console window open
+    return 0;
 }
